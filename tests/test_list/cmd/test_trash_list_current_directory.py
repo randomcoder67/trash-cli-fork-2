07b48cda5660a2723660003f6a512cd1882b8650
randomcoder67 <randomcoder67@gmail.com>
2024-07-11 16:23:21 +0100

    added tests using a different volume

--- tests/test_list/cmd/test_trash_list_current_directory.py
+++ tests/test_list/cmd/test_trash_list_current_directory.py
@@ -14,6 +14,13 @@ class TestTrashListCurrentDirectory:
         u = trash_list_user
         u.set_fake_cwd("/home/user/currentdir")
         return u
+    
+    @pytest.fixture
+    def user_volume(self, trash_list_user):
+        u = trash_list_user
+        u.add_disk("disk")
+        u.set_fake_cwd("/disk/dir1")
+        return u
                                    
     def test_currentdir_files_only(self, user):
         user.home_trash_dir().add_trashinfo2('/home/user/currentdir/file1',
@@ -45,6 +52,42 @@ class TestTrashListCurrentDirectory:
                                                datetime(2001, 2, 3, 23, 55, 59))
 
         result = user.run_trash_list('--currentdir')
+
+        assert_equals_with_unidiff("",
+                                   result.stdout)
+    
+    # Same tests but using a different disk
+    
+    def test_currentdir_files_only(self, user_volume):
+        user_volume.home_trash_dir().add_trashinfo2('/disk/dir1/file1',
+                                               datetime(2001, 2, 3, 23, 55, 59))
+        user_volume.home_trash_dir().add_trashinfo2('/disk/dir2/file2',
+                                               datetime(2001, 2, 3, 23, 55, 59))
+
+        result = user_volume.run_trash_list('--currentdir')
+
+        assert_equals_with_unidiff("2001-02-03 23:55:59 " + "/disk/dir1/file1\n",
+                                   result.stdout)
+
+    def test_currentdir_nested_folders(self, user_volume):
+        user_volume.home_trash_dir().add_trashinfo2('/disk/dir2/dir1/file1',
+                                               datetime(2001, 2, 3, 23, 55, 59))
+        user_volume.home_trash_dir().add_trashinfo2('/disk/dir1/dir2/file2',
+                                               datetime(2001, 2, 3, 23, 55, 59))
+
+        result = user_volume.run_trash_list('--currentdir')
+
+        assert_equals_with_unidiff("2001-02-03 23:55:59 " + "/disk/dir1/dir2/file2\n",
+                                   result.stdout)
+
+
+    def test_should_output_currendir_should_not_show_currentdir_itself(self, user_volume):
+        user_volume.home_trash_dir().add_trashinfo2("/disk/dir1",
+                                               datetime(2001, 2, 3, 23, 55, 59))
+        user_volume.home_trash_dir().add_trashinfo2('/disk1/dir2/file1',
+                                               datetime(2001, 2, 3, 23, 55, 59))
+
+        result = user_volume.run_trash_list('--currentdir')
 
         assert_equals_with_unidiff("",
                                    result.stdout)
